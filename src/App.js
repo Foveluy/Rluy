038c66391cd1246f421a93c25291f06fbd9dbd11
方正 <215566435@qq.com>
2017-12-09 12:39:33 +1300

    无限Carousel组件

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import { Button } from 'antd-mobile';
-import logo from './logo.svg';
+import { Carousel } from './Carousel/index';
 import './App.css';
-
-import { Test } from 'test'
-import { j } from 'test/abc'
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button >default disabled</Button>
-        <Test />
+        <h1>我靠，Carousel组件</h1>
+        <Carousel>
+          <div>一</div>
+          <div>二</div>
+          <div>三</div>
+          <div>四</div>
+        </Carousel>
       </div>
     );
   }
--- src/test/abc.js
+++
@@ -1,1 +0,0 @@
-export const j = 'anc'
