ea6629561f99582aadb59cf4e4109423afdb2253
Z F <215566435@qq.com>
2018-04-11 17:32:38 +1200

    新增载入loader.

--- RluyLoader.js
+++ RluyLoader.js
@@ -6,14 +6,21 @@ const wrapper = source => {
   return `_Rluy2.default.model(require("${source}"));\n`
 }
 
+const wrapperLayout = (key, componentPath) => {
+  return `\n _Rluy2.default.routingComponent[${key}]=require("${componentPath}");\n`
+}
+
 module.exports = function(source) {
   this.cacheable(false)
 
+  console.log('先走到这里哦')
   try {
     var appPath = path.resolve('src/model')
+    var layoutPath = path.resolve('src/page')
     const dirs = fs.readdirSync(appPath)
     this.addContextDependency(appPath)
     appPath = appPath + '/'
+    layoutPath = layoutPath + '/'
     var src = fs.readdirSync(appPath).reduce(function(pre, next, index) {
       const prePath = appPath + pre
       const nextPath = appPath + next
@@ -23,12 +30,24 @@ module.exports = function(source) {
       return wrapper(prePath)
     })
 
+    var src2 = fs.readdirSync(layoutPath).reduce(function(pre, next, index) {
+      const prePath = layoutPath + pre
+      const nextPath = layoutPath + next
+      if (index === 1) {
+        return wrapperLayout(index, prePath) + wrapperLayout(index + '123', nextPath)
+      }
+      return pre+ wrapperLayout(index, nextPath)
+    })
+
     if (dirs.length === 1) {
+      src2 = wrapperLayout(1, layoutPath + src2)
       src = wrapper(appPath + src)
     }
 
+    console.log(chalk.green(src2))
+
     console.log(chalk.green('adding module...'))
-    return source + '\n' + src
+    return source + '\n' + src + src2
   } catch (e) {
     console.log(chalk.red('please create a module folder in `{your app}/src/`'))
     throw e
